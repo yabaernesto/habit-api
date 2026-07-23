fbf3341536485a63459920a3d64e59a2f76c2a18
Yaba Ernesto <yabaernesto@gmail.com>
2026-07-23 02:03:14 +0100

    chore: prisma erd e seed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const url = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({ connectionString });
+const adapter = new PrismaBetterSqlite3({ url });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
