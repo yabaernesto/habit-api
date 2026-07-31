f7698e9da588e564657f0db0762747791fd3f6d7
Yaba Ernesto <yabaernesto@gmail.com>
2026-07-31 20:28:37 +0100

    feat: rota de completar hábito

--- src/routes.ts
+++ src/routes.ts
@@ -74,4 +74,58 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     };
   });
+
+  // completar / não completar hábito
+  app.patch("/habits/:id/toggle", async (request, reply) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    // se o dia não existir, criar
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    // buscar hábito que foi marcado como completo
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    // se o hábito já estiver marcado como completo, remover a marcação, caso contrário, marcar como completo
+    if (dayHabit) {
+      // remover a marcação de completo
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      // completar o hábito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
 }
